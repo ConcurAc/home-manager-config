804a850f6d5f337f40c2083a71e561d9dfc25608
Connor Davis <concurac@proton.me>
2025-08-30 19:20:05 +1000

    Created module for music related packages

---
+++ modules/music.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+{
+  home.packages =
+    with pkgs;
+    [
+      reaper
+      infamousPlugins
+
+      vital
+      aether-lv2
+      dragonfly-reverb
+      mod-distortion
+      quadrafuzz
+      fire
+      guitarix
+      carla
+
+      yabridge
+      yabridgectl
+    ]
+    ++ (with magnetophonDSP; [
+      shelfMultiBand
+      pluginUtils
+      faustCompressors
+      VoiceOfFaust
+      RhythmDelay
+      MBdistortion
+      LazyLimiter
+      CompBus
+      CharacterCompressor
+    ]);
+}
