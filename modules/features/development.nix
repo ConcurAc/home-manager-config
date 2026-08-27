b4036179f9ac83c47b2351c9ec63f4657d7f0eb9
Connor Davis <connor@scequ.com>
2026-08-27 12:15:42 +1000

    fix: add LSPs and dont update env unless config is enabled

--- modules/features/development.nix
+++ modules/features/development.nix
@@ -11,6 +11,10 @@ in
   options.features.development = {
     rust = {
       enable = lib.mkEnableOption "Configure rust development toolchain";
+      enableLSP = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+      };
       cargoHome = lib.mkOption {
         type = lib.types.str;
         default = "$HOME/.local/share/cargo";
@@ -22,6 +26,10 @@ in
     };
     go = {
       enable = lib.mkEnableOption "Configure go development toolchain";
+      enableLSP = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+      };
     };
     arduino = {
       enable = lib.mkEnableOption "Configure arduino development toolchain";
@@ -35,13 +43,15 @@ in
   config = {
     home = {
       sessionVariables = {
-        CARGO_HOME = cfg.rust.cargoHome;
-        RUSTUP_HOME = cfg.rust.rustupHome;
-        ARDUINO_DIRECTORIES_DATA = cfg.arduino.arduinoHome;
+        CARGO_HOME = lib.mkIf cfg.rust.enable cfg.rust.cargoHome;
+        RUSTUP_HOME = lib.mkIf cfg.rust.enable cfg.rust.rustupHome;
+        ARDUINO_DIRECTORIES_DATA = lib.mkIf cfg.arduino.enable cfg.arduino.arduinoHome;
       };
       packages =
         (lib.optional (cfg.rust.enable) pkgs.cargo)
+        ++ (lib.optional (cfg.rust.enable && cfg.rust.enableLSP) pkgs.rust-analyzer)
         ++ (lib.optional (cfg.go.enable) pkgs.go)
+        ++ (lib.optional (cfg.go.enable && cfg.go.enableLSP) pkgs.gopls)
         ++ (lib.optional (cfg.arduino.enable) pkgs.arduino-cli);
     };
   };
